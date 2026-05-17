3890f6a34d1d16cefe24affc25fadaf9a6953a9f
wonnie901-spec <wonnie901@gmail.com>
2026-05-17 14:41:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+st.set_page_config(
+    page_title="AI Scam Detector",
+    layout="centered"
+    
+st.markdown(" " "
+     <style>
+     @import url('https://fonts.googleapis.com/css2?family=CatchyMager:wght@400;600&display=swap')
+
+html, body, [class*="css"]  {
+        font-family: 'Catchy Mager', sans-serif;
+}
+</style>
+" " ",
+unsafe_allow_html=True
+)
+
+st.markdown(
+    " " "
+    body  {
+           background-color: #073763ff; /* dark blue 3 */
+    }
+    </style>
+    " " ",
+    unsafe_allow_html=True
+)
 import re
 import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
